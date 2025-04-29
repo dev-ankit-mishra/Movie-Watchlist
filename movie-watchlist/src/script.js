ae9afdde89d241fc3e727d56e24c77c63445768a
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-04-29 23:06:01 +0530

    added movie fetching program

--- movie-watchlist/src/script.js
+++ movie-watchlist/src/script.js
@@ -1,5 +1,34 @@
-fetch("")
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data)
-    })
+const MOVIE_KEY=import.meta.env.VITE_MOVIE_KEY;
+
+
+
+document.querySelector("#search").addEventListener("click", event => {
+    event.preventDefault();
+    let movie
+    const title=document.querySelector("#title").value;
+    fetch(`http://www.omdbapi.com/?apikey=${MOVIE_KEY}&t=${title}`)
+        .then(res=>res.json())
+        .then(data=>{
+            movie=data;
+            document.querySelector(".movies-container").innerHTML=`
+        <img src="${movie.Poster}" alt="movie-img"/>
+        <div class="movie-details">
+            <p class="movie-title">
+                <span>${movie.Title}</span>
+                <span>&starf; ${movie.Ratings[0].Value}</span>
+             </p>
+             <p class="movie-info">
+                <span>${movie.Runtime}</span>
+                <span>${movie.Genre}</span>
+                <span>watchList</span>
+             </p>
+            <p>${movie.Plot}</p>
+        </div>
+    `
+        })
+
+
+})
+
+
+
